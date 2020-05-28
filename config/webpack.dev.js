6caa1df14873e8efda14814d7a708d184b537e69
Ernesto Jara Olveda <ernesto.jara06@gmail.com>
2020-05-28 12:12:18 -0500

    ignore

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,119 +2,150 @@
 
 const path = require("path");
 const webpack = require("webpack");
+const PnpWebpackPlugin = require("pnp-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
-const InlineChunkHtmlPlugin = require("react-dev-utils/InlineChunkHtmlPlugin");
 const ManifestPlugin = require("webpack-manifest-plugin");
 const InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
 const WatchMissingNodeModulesPlugin = require("react-dev-utils/WatchMissingNodeModulesPlugin");
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
+const paths = require("./paths");
+const modules = require("./modules");
+const getClientEnvironment = require("./env");
 const ModuleNotFoundPlugin = require("react-dev-utils/ModuleNotFoundPlugin");
-const vars = require('./var');
-const postcssNormalize = require("postcss-normalize");
-const env = vars();
 
-const imageInlineSizeLimit = 1000;
+const postcssNormalize = require("postcss-normalize");
+const appPackageJson = require(paths.appPackageJson);
+const shouldUseSourceMap = false;
+const imageInlineSizeLimit = parseInt(process.env.IMAGE_INLINE_SIZE_LIMIT || "10000");
+const env = getClientEnvironment("");
+
 module.exports = {
     mode: "development",
     bail: false,
     devtool: "source-map",
-    name: "client",
-    target: "web",
-    entry: [
-        'webpack/hot/poll?100',
-        require.resolve("@babel/register"),
-        require.resolve("core-js"),
-        require.resolve("@babel/runtime/regenerator"),
-        require.resolve("react-hot-loader/patch"),
-        require.resolve("es6-promise/auto"),
-        require.resolve(path.join(__dirname, '../src/dev.ts'))
-    ],
+    entry: paths.appIndexJs,
     output: {
-        path:  path.resolve(__dirname, "../public/"),
+        path: undefined,
         pathinfo: true,
-        filename: "static/js/[name]-bundle.js",
+        filename: "static/js/bundle.js",
         futureEmitAssets: true,
         chunkFilename: "static/js/[name].chunk.js",
-        publicPath: "/",
-        devtoolModuleFilenameTemplate: info =>
-            path.resolve(info.absoluteResourcePath).replace(/\\/g, "/"),
-        jsonpFunction: `webpackJsonp-eneto-app`,
-        globalObject: "this",
+        publicPath: paths.publicUrlOrPath,
+        devtoolModuleFilenameTemplate: (info) => path.resolve(info.absoluteResourcePath).replace(/\\/g, "/"),
+        jsonpFunction: `webpackJsonp${appPackageJson.name}`,
+        globalObject: "this"
     },
-    devServer: {
-        port: Number(process.env.PORT),
-        contentBase: 'public',
-        hot: true,
-        stats:{
-            colors: true
+    optimization: {
+        minimize: false,
+        splitChunks: {
+            chunks: "all",
+            name: false
         },
-        quiet: false,
-        host: "localhost",
-        overlay: true,
-        historyApiFallback: {
-            disableDotRule: true,
-        },
+        runtimeChunk: {
+            name: (entrypoint) => `runtime-${entrypoint.name}`
+        }
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js"],
+        modules: ["node_modules", paths.appNodeModules].concat(modules.additionalModulePaths || []),
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        alias: {
+            "react-native": "react-native-web",
+            ...(modules.webpackAliases || {})
+        },
+        plugins: [PnpWebpackPlugin, new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson])]
+    },
+    resolveLoader: {
+        plugins: [PnpWebpackPlugin.moduleLoader(module)]
     },
     module: {
+        strictExportPresence: true,
         rules: [
+            { parser: { requireEnsure: false } },
             {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: [{ loader: 'babel-loader' }]
-            },
-            {
-                test: /\.(scss)$/,
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
+                oneOf: [
                     {
-                        loader: 'postcss-loader',
+                        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                        loader: require.resolve("url-loader"),
                         options: {
-                            ident: "postcss",
-                            plugins: () => [
-                                require("postcss-flexbugs-fixes"),
-                                require("postcss-preset-env")({
-                                    autoprefixer: {
-                                        flexbox: "no-2009",
-                                    },
-                                    stage: 3,
-                                }),
-
-                                postcssNormalize(),
-                            ],
-                            sourceMap: true,
+                            limit: imageInlineSizeLimit,
+                            name: "static/media/[name].[hash:8].[ext]"
                         }
                     },
-                    { loader: 'sass-loader' },
+                    {
+                        test: /\.(js|mjs|jsx|ts|tsx)$/,
+                        include: paths.appSrc,
+                        loader: require.resolve("babel-loader"),
+                        options: {
+                            customize: require.resolve("babel-preset-react-app/webpack-overrides"),
+
+                            plugins: [
+                                [
+                                    require.resolve("babel-plugin-named-asset-import"),
+                                    {
+                                        loaderMap: {
+                                            svg: {
+                                                ReactComponent: "@svgr/webpack?-svgo,+titleProp,+ref![path]"
+                                            }
+                                        }
+                                    }
+                                ]
+                            ],
+                            cacheDirectory: true,
+                            cacheCompression: false,
+                            compact: false
+                        }
+                    },
+                    {
+                        test: /\.(js|mjs)$/,
+                        exclude: /@babel(?:\/|\\{1,2})runtime/,
+                        loader: require.resolve("babel-loader"),
+                        options: {
+                            babelrc: false,
+                            configFile: false,
+                            compact: false,
+                            presets: [[require.resolve("babel-preset-react-app/dependencies"), { helpers: true }]],
+                            cacheDirectory: true,
+                            cacheCompression: false,
+                            sourceMaps: shouldUseSourceMap,
+                            inputSourceMap: shouldUseSourceMap
+                        }
+                    },
+                    {
+                        test: /\.(scss)$/,
+                        use: [
+                            { loader: "style-loader" },
+                            { loader: "css-loader" },
+                            {
+                                loader: "postcss-loader",
+                                options: {
+                                    ident: "postcss",
+                                    plugins: () => [
+                                        require("postcss-flexbugs-fixes"),
+                                        require("postcss-preset-env")({
+                                            autoprefixer: {
+                                                flexbox: "no-2009"
+                                            },
+                                            stage: 3
+                                        }),
+
+                                        postcssNormalize()
+                                    ],
+                                    sourceMap: true
+                                }
+                            },
+                            { loader: "sass-loader" }
+                        ]
+                    },
+                    {
+                        loader: require.resolve("file-loader"),
+                        exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+                        options: {
+                            name: "static/media/[name].[hash:8].[ext]"
+                        }
+                    }
                 ]
             }
-            ,
-            {
-                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-                loader: require.resolve("url-loader"),
-                options: {
-                    limit: imageInlineSizeLimit,
-                    name: "static/media/[name].[ext]",
-                },
-            },
-            {
-                loader: require.resolve("file-loader"),
-
-                exclude: [
-                    /\.(js|mjs|jsx|ts|tsx)$/,
-                    /\.html$/,
-                    /\.json$/,
-                    /\.scss$/,
-                    /\.css$/,
-                ],
-                options: {
-                    name: "static/media/[name].[ext]",
-                },
-            },
-
         ]
     },
     plugins: [
@@ -123,47 +154,35 @@ module.exports = {
                 {},
                 {
                     inject: true,
-                    template: path.join(__dirname, '../public/index.html'),
-                }
+                    template: paths.appHtml
+                },
+                undefined
             )
         ),
-
-        new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime-.+[.]js/]),
-
         new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
-
-        new ModuleNotFoundPlugin(path.join(__dirname, "../src/")),
-
+        new ModuleNotFoundPlugin(paths.appPath),
         new webpack.DefinePlugin(env.stringified),
-
         new webpack.HotModuleReplacementPlugin(),
-
         new CaseSensitivePathsPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new WatchMissingNodeModulesPlugin(path.join(__dirname, "../node_modules/")),
-
+        new WatchMissingNodeModulesPlugin(paths.appNodeModules),
         new ManifestPlugin({
             fileName: "asset-manifest.json",
-            publicPath: process.env.PUBLIC_URL,
+            publicPath: paths.publicUrlOrPath,
             generate: (seed, files, entrypoints) => {
                 const manifestFiles = files.reduce((manifest, file) => {
                     manifest[file.name] = file.path;
                     return manifest;
                 }, seed);
-                const entrypointFiles = entrypoints.main.filter(
-                    fileName => !fileName.endsWith(".map")
-                );
+                const entrypointFiles = entrypoints.main.filter((fileName) => !fileName.endsWith(".map"));
 
                 return {
                     files: manifestFiles,
-                    entrypoints: entrypointFiles,
+                    entrypoints: entrypointFiles
                 };
-            },
+            }
         }),
-
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
     ].filter(Boolean),
-
     node: {
         module: "empty",
         dgram: "empty",
@@ -172,8 +191,7 @@ module.exports = {
         http2: "empty",
         net: "empty",
         tls: "empty",
-        child_process: "empty",
+        child_process: "empty"
     },
-
-    performance: false,
-}
+    performance: false
+};
